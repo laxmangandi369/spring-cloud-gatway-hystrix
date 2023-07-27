eaaabaa0a9f0959c189e0a831bf82503ef50b44d
laxmangandi369 <119059799+laxmangandi369@users.noreply.github.com>
2023-07-27 18:55:30 +0530

    Update CloudConfigServerApplication.java

--- cloud-config-server/src/main/java/com/javatechie/cloud/config/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/javatechie/cloud/config/CloudConfigServerApplication.java
@@ -12,6 +12,25 @@ public class CloudConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigServerApplication.class, args);
+		
+===============
+Github Testing
+===============
+-----for public access repository-----
+
+1. Project is getting scanned.
+2. After every commit & Pull request project is getting rescanned. 
+3. we can see errors, duplications etc.
+4. we can see activity log & progress tab issues.
+===============
+Github Testing
+===============
+-----for public access repository-----
+
+1. Project is getting scanned.
+2. After every commit & Pull request project is getting rescanned. 
+3. we can see errors, duplications etc.
+4. we can see activity log & progress tab issues.
 	}
 
 }
